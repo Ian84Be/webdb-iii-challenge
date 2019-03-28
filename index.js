b0cc50a50ea6b3ecb2f1fe29a85a1812a770eb88
Ian Be <ian84be@gmail.com>
2019-03-27 19:51:49 -0500

    STRETCH: students-router.js

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ server.use(helmet());
 server.use(express.json());
 
 const cohortsRouter = require('./cohorts-router.js');
+const studentsRouter = require('./students-router.js');
 
 server.use('/api/cohorts', cohortsRouter);
+server.use('/api/students', studentsRouter);
 
 server.listen(5000, () => {
     console.log('LISTEN 5000');
--- students-router.js
+++ students-router.js
@@ -5,12 +5,12 @@ const db = knex(knexConfig.development);
 const router = require('express').Router();
 
 router.post('/', async (req,res) => {
-    if (!req.body.name) {
-        res.status(400).json({error:'missing required field: name'});
+    if (!req.body.name || !req.body.cohort_id) {
+        res.status(400).json({error:'missing name / cohort id'});
     } else {
         try {
-            const cohort = await db('cohorts').insert(req.body);
-            res.status(200).json(cohort);
+            const student = await db('students').insert(req.body);
+            res.status(200).json(student);
         }
         catch(err) {
             res.status(500).json({error:err});
@@ -21,8 +21,8 @@ router.post('/', async (req,res) => {
 
 router.get('/', async (req,res) => {
     try {
-        const cohorts = await db('cohorts');
-        res.status(200).json(cohorts);
+        const students = await db('students');
+        res.status(200).json(students);
     }
     catch(err) {
         res.status(500).json({error:err});
@@ -32,20 +32,15 @@ router.get('/', async (req,res) => {
 router.get('/:id', async (req,res) => {
     const {id} = req.params;
     try {
-        const cohort = await db('cohorts').where({id}).first();
-        res.status(200).json(cohort);
-    }
-    catch(err) {
-        res.status(500).json(err);
-    }
-});
-
-router.get('/:id/students', async (req,res) => {
-    const {id} = req.params;
-    try {
-        const students = await 
-        db('students').where({cohort_id: id});
-        res.status(200).json(students);
+        const student = await
+        db.select('students.id','students.name','cohorts.name as cohort').from('students')
+        .leftJoin('cohorts',{'students.cohort_id':'cohorts.id'})
+        .where({'students.id':id}).first();
+        if (student) {
+            res.status(200).json(student);
+        } else {
+            res.status(404).json({error:'ID not found'});
+        }
     }
     catch(err) {
         res.status(500).json(err);
@@ -54,14 +49,14 @@ router.get('/:id/students', async (req,res) => {
 
 router.put('/:id', async (req,res) => {
     const {id} = req.params;
-    if (!req.body.name) {
-        res.status(400).json({error:'missing required field: name'});
+    if (!req.body.name || !req.body.cohort_id) {
+        res.status(400).json({error:'missing name / cohort_id'});
     } else {
         try {
-            const cohort = await
-            db('cohorts').where({id}).update(req.body);
-            if (cohort) {
-                const result = await db('cohorts').where({id}).first();
+            const student = await
+            db('students').where({id}).update(req.body);
+            if (student) {
+                const result = await db('students').where({id}).first();
                 res.status(200).json(result);
             }
             else {
@@ -77,8 +72,8 @@ router.put('/:id', async (req,res) => {
 router.delete('/:id', async (req,res) => {
     const {id} = req.params;
     try {
-        const cohort = await db('cohorts').where({id}).del();
-        if (cohort) {
+        const student = await db('students').where({id}).del();
+        if (student) {
             res.status(200).json({success: 'Record deleted'});
         } else {
             res.status(404).json({ error: 'Record not found' });
@@ -88,5 +83,6 @@ router.delete('/:id', async (req,res) => {
         res.status(500).json(err);
     }
 });
+
 
 module.exports = router;
